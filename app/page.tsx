b5ca8c080cfa1b85d9f01ce70d43ebe707262b6e
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2025-06-10 02:18:05 +0200

    Home Page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard"
+import CompanionsList from "@/components/CompanionsList"
+import Cta from "@/components/CTA"
+import { recentSessions } from "@/constants"
 
 const Home = () => {
   return (
-    <div className='text-2xl'>Page</div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id="1"
+          name="Sample Companion"
+          topic="Sample Topic"
+          subject="Sample Subject"
+          duration={45}
+          color="#ff4950"
+          bookmarked
+        />
+        <CompanionCard
+          id="2"
+          name="Sample Companion"
+          topic="Sample Topic"
+          subject="Sample Subject"
+          duration={45}
+          color="#ffeec1"
+          bookmarked
+        />
+        <CompanionCard
+          id="3"
+          name="Sample Companion"
+          topic="Sample Topic"
+          subject="Sample Subject"
+          duration={45}
+          color="#ffdf40"
+          bookmarked
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList title="Recently completed sessions" companions={recentSessions} classNames="w-2/3 max-lg:w-full mb-3" />
+        <Cta />
+      </section>
+
+    </main>
   )
 }
 

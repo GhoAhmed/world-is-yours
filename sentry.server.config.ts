81dc786fe6ecefe82d35b369a71b3f0d97fbab8c
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2025-06-11 18:13:59 +0200

    campanion component & sentry config

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://189721dbc2657f5e4f45cf3946e4e74b@o4509470780424192.ingest.de.sentry.io/4509480872116304",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
